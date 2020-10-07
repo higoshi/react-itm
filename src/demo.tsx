783830aaf00a1c64d28b6c39beb9490c4a643866
higoshi <h33je5sg@gmail.com>
2020-10-08 01:06:56 +0900

    demo

---
+++ src/demo.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import * as ReactDOM from 'react-dom';
+import {ITM} from './ITM';
+
+const element = document.getElementById('app');
+ReactDOM.render(<>
+  <ITM token="aaa" cid={1234} gid="goihpj["></ITM>
+</>, element);
